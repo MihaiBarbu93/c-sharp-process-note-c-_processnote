dd1d68a3351e49af8c9292a0ff169f08266f9089
Papcio Chmiel <pfaderewski@gmail.com>
2019-09-11 15:21:21 +0200

    memory usage for each thread probably done, cpu still in progress

--- ProcessNote/ProcessNote/MainWindow.xaml.cs
+++ ProcessNote/ProcessNote/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+
 
 namespace ProcessNote
 {
@@ -21,22 +24,95 @@ namespace ProcessNote
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Process> _processess;
 
-        public MainWindow()
+        private List<Process> _processess; 
+
+
+
+    public MainWindow()
         {
             InitializeComponent();
             //DataContext = new MainWindowViewModel();
 
-            _processess = new List<Process>();
+            Process[] processlist = Process.GetProcesses();
+
+            /*foreach (Process theprocess in processlist)
+            {
+                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+                _processess.Add(new Process { PID = theprocess.Id, Name = theprocess.ProcessName });
+            }*/
+
+
+            _processess = new List<Process>(processlist);
 
             processTable.ItemsSource = _processess;
 
-            _processess.Add(new Process { PID = 1, Name = "Test1" });
-            _processess.Add(new Process { PID = 2, Name = "Test2" });
-            _processess.Add(new Process { PID = 3, Name = "Test3" });
-            _processess.Add(new Process { PID = 4, Name = "Test4" });
-            _processess.Add(new Process { PID = 5, Name = "new" });
+
+
+
+        }
+        public class DataGridViewCellEventArgs : EventArgs
+        {
+
+        }
+
+
+
+
+    private void dataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender != null)
+            {
+                DataGrid grid = sender as DataGrid;
+                if (grid != null && grid.SelectedItems != null && grid.SelectedItems.Count == 1)
+                {
+                    DataGridRow dgr = grid.ItemContainerGenerator.ContainerFromItem(grid.SelectedItem) as DataGridRow;
+                    Process obj = dgr.Item as Process;
+                    var name = string.Empty;
+
+                    foreach (var instance in new PerformanceCounterCategory("Process").GetInstanceNames())
+                    {
+                        if (instance.StartsWith(obj.ProcessName))
+                        {
+                            using (var processId = new PerformanceCounter("Process", "ID Process", instance, true))
+                            {
+                                if (obj.Id == (int)processId.NextValue())
+                                {
+                                    name = instance;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    var cpuUsage = new PerformanceCounter("Process", "% Processor Time", name, true);
+                    var ramUsage = new PerformanceCounter("Process", "Working Set - Private", name, true);
+
+
+                    // Getting first initial values
+                    //cpuUsage.NextValue();
+                    //ramUsage.NextValue();
+
+                    //Thread.Sleep(500);
+   
+                    //curTime = DateTime.Now;
+
+                    //PerformanceCounter myAppCpu = new PerformanceCounter("ID Process", "% Processor Time", obj.Id.ToString(), true);
+                    //var ramUsage = new PerformanceCounter("Process", "Working Set - Private", obj.ProcessName.ToString(), true);
+                    //double pct = myAppCpu.RawValue;
+
+                    var specifier = "P";
+
+                    string id = obj.Id.ToString();
+                    //CPU.Text = id;
+
+                    StartTime.Text = obj.StartTime.ToUniversalTime().ToString("HH:mm:ss");
+                    //CPU.Text = (pct/10000).ToString(specifier);
+                    CPU.Text = Math.Round(cpuUsage.NextValue() / Environment.ProcessorCount, 2).ToString(specifier);
+                    MemoryUsage.Text = (ramUsage.NextValue() / 1024).ToString();
+                    RunningTime.Text = obj.UserProcessorTime.ToString();
+                }
+            }
         }
 
     }
