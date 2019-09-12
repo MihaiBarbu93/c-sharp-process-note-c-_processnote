bc1d409e6334d408eb09c2a185003af95135df2f
mwagrodzki <m.wagrodzki@gmail.com>
2019-09-12 11:35:49 +0100

    SimpleProcess class

--- ProcessNote/ProcessNote/MainWindow.xaml.cs
+++ ProcessNote/ProcessNote/MainWindow.xaml.cs
@@ -24,46 +24,30 @@ namespace ProcessNote
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        private List<Process> _processess; 
-
-
-
-    public MainWindow()
+        private List<SimpleProcess> _processess;
+        
+        public MainWindow()
         {
             InitializeComponent();
-            //DataContext = new MainWindowViewModel();
-
-            Process[] processlist = Process.GetProcesses();
-            //ProcessThreadCollection[] threadslist;
-     
-
-            /*foreach (Process theprocess in processlist)
-            {
-                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
-                _processess.Add(new Process { PID = theprocess.Id, Name = theprocess.ProcessName });
-            }*/
-
-
-            _processess = new List<Process>(processlist);
-    
-
-            processTable.ItemsSource = _processess;
-  
-
+            getProcesses();
         }
         public class DataGridViewCellEventArgs : EventArgs
         {
 
         }
 
+        private void getProcesses()
+        {
+            Process[] processlist = Process.GetProcesses();
+            _processess = new List<SimpleProcess>();
 
-        private static DateTime lastTime;
-        private static TimeSpan lastTotalProcessorTime;
-        private static DateTime curTime;
-        private static TimeSpan curTotalProcessorTime;
-
-
+            foreach (Process theprocess in processlist)
+            {
+                _processess.Add(new SimpleProcess(theprocess));
+            }
+            processTable.ItemsSource = _processess;
+        }
+               
         private void dataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (sender != null)
@@ -72,80 +56,14 @@ namespace ProcessNote
                 if (grid != null && grid.SelectedItems != null && grid.SelectedItems.Count == 1)
                 {
                     DataGridRow dgr = grid.ItemContainerGenerator.ContainerFromItem(grid.SelectedItem) as DataGridRow;
-                    Process obj = dgr.Item as Process;
-                    var name = string.Empty;
+                    SimpleProcess process = dgr.Item as SimpleProcess;
 
-                    foreach (var instance in new PerformanceCounterCategory("Process").GetInstanceNames())
-                    {
-                        if (instance.StartsWith(obj.ProcessName))
-                        {
-                            using (var processId = new PerformanceCounter("Process", "ID Process", instance, true))
-                            {
-                                if (obj.Id == (int)processId.NextValue())
-                                {
-                                    name = instance;
-                                    break;
-                                }
-                            }
-                        }
-                    }
+                    threadsTable.ItemsSource = process.ThreadsList;
 
-                    ProcessThread[] threadslist = obj.Threads.Cast<ProcessThread>().ToArray();
-                    //_threads = new List<ProcessThread>(threadslist);
-                    threadsTable.ItemsSource = threadslist;//_threads;
-
-                    var cpuUsage = new PerformanceCounter("Process", "% Processor Time", name, true);
-                    var ramUsage = new PerformanceCounter("Process", "Working Set - Private", name, true);
-
-                    double CPUUsage = 0;
-                    //Process[] pp = Process.GetProcessById(name);
-                    //Process p = pp[0];
-
-                    Process p = obj;
-       
-                    if (lastTime == null || lastTime == new DateTime())
-                    {
-                        lastTime = DateTime.Now;
-                        lastTotalProcessorTime = p.TotalProcessorTime;
-                    }
-                    else
-                    {
-                        curTime = DateTime.Now;
-                        curTotalProcessorTime = p.TotalProcessorTime;
-
-                        CPUUsage = (curTotalProcessorTime.TotalMilliseconds - lastTotalProcessorTime.TotalMilliseconds) / curTime.Subtract(lastTime).TotalMilliseconds / Convert.ToDouble(Environment.ProcessorCount);
-                        //Console.WriteLine("{0} CPU: {1:0.0}%", name, CPUUsage * 100);
-
-                        lastTime = curTime;
-                        lastTotalProcessorTime = curTotalProcessorTime;
-                    }
-
-                    // Getting first initial values
-                    //cpuUsage.NextValue();
-                    //ramUsage.NextValue();
-
-                    //Thread.Sleep(500);
-
-                    //curTime = DateTime.Now;
-
-                    //PerformanceCounter myAppCpu = new PerformanceCounter("ID Process", "% Processor Time", obj.Id.ToString(), true);
-                    //var ramUsage = new PerformanceCounter("Process", "Working Set - Private", obj.ProcessName.ToString(), true);
-                    //double pct = myAppCpu.RawValue;
-
-                    var specifier = "P";
-
-
-                    string id = obj.Id.ToString();
-                    //CPU.Text = id;
-
-                    StartTime.Text = obj.StartTime.ToString("HH:mm:ss");
-                    //CPU.Text = (pct/10000).ToString(specifier);
-                    //CPU.Text = Math.Round(cpuUsage.NextValue() / Environment.ProcessorCount, 2).ToString();
-                    //CPU.Text = (cpuUsage.RawValue / Environment.ProcessorCount).ToString();
-                    //CPU.Text = (cpuUsage.RawValue/Environment.ProcessorCount/10000).ToString();
-                    CPU.Text = (CPUUsage).ToString(specifier);
-                    MemoryUsage.Text = (ramUsage.NextValue() / 1024).ToString("0.00") + " KB";
-                    RunningTime.Text = obj.UserProcessorTime.ToString();
+                    StartTime.Text = process.StartTime;
+                    CPU.Text = process.getCPU_Usage();
+                    MemoryUsage.Text = process.getRamUsage();
+                    RunningTime.Text = process.ElapsedTime;
                 }
             }
         }
---
+++ ProcessNote/ProcessNote/SimpleProcess.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProcessNote
+{
+    class SimpleProcess
+    {
+
+        public int PID { get { return process.Id; } }
+        public string Name { get { return process.ProcessName; } }
+        public string StartTime { get; }
+        public string ElapsedTime { get; }
+        public ProcessThread[] ThreadsList { get { return process.Threads.Cast<ProcessThread>().ToArray(); } }
+
+        private DateTime lastTime;
+        private TimeSpan lastTotalProcessorTime;
+        private Process process;
+
+        public SimpleProcess(Process process)
+        {
+            try
+            {
+                StartTime = process.StartTime.ToString("HH:mm:ss");
+            }
+            catch(Exception e)
+            {
+                StartTime = "N/A";
+            }
+            try
+            {
+                ElapsedTime = process.UserProcessorTime.ToString();
+            }
+            catch(Exception e)
+            {
+                ElapsedTime = "N/A";
+            }
+            lastTime = DateTime.Now;
+            try
+            {
+                lastTotalProcessorTime = process.TotalProcessorTime;
+            }
+            catch(Exception e)
+            {
+                lastTotalProcessorTime = TimeSpan.Zero;
+            }
+            this.process = process;
+        }
+
+        public string getCPU_Usage()
+        {
+            TimeSpan curTotalProcessorTime;
+            try
+            {
+                curTotalProcessorTime = process.TotalProcessorTime;
+            }
+            catch(Exception e)
+            {
+                return "N/A";
+            }
+            DateTime curTime = DateTime.Now;
+            string instanceName = getInstanceName();
+            if (instanceName.Equals("N/A"))
+            {
+                return "N/A";
+            }
+            var cpuUsage = new PerformanceCounter("Process", "% Processor Time", instanceName, true);
+
+            double CPUUsage = (curTotalProcessorTime.TotalMilliseconds - lastTotalProcessorTime.TotalMilliseconds)
+                / curTime.Subtract(lastTime).TotalMilliseconds
+                / Convert.ToDouble(Environment.ProcessorCount);
+
+            lastTime = curTime;
+            lastTotalProcessorTime = curTotalProcessorTime;
+
+            return (CPUUsage).ToString("P");
+        }
+
+        public string getRamUsage()
+        {
+            string instanceName = getInstanceName();
+            if (instanceName.Equals("N/A"))
+            {
+                return "N/A";
+            }
+            var ramUsage = new PerformanceCounter("Process", "Working Set - Private", instanceName, true);
+            return (ramUsage.NextValue() / 1048576).ToString("0.00") + " MB";
+        }
+
+        private string getInstanceName()
+        {
+            foreach (var instanceName in new PerformanceCounterCategory("Process").GetInstanceNames())
+            {
+                if (instanceName.StartsWith(Name))
+                {
+                    using (var processPerformance = new PerformanceCounter("Process", "ID Process", instanceName, true))
+                    {
+                        try
+                        {
+                            if (PID == (int)processPerformance.RawValue)
+                            {
+                                return instanceName;
+                            }
+                        }
+                        catch(InvalidOperationException e)
+                        {
+                            return "N/A";
+                        }
+                            
+                    }
+                }
+            }
+            return "N/A";
+        }
+    }
+}
